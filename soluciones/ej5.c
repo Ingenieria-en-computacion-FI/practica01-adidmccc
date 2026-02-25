d85e5f2484930fefe61b3087253bfced72f1b5e6
adidmccc <adieldan.1912@gmail.com>
2026-02-24 20:53:43 -0600

    Ejercicio5

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -1,6 +1,14 @@
+#include <stdio.h> 
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 5
-    return 0;
+int main() {
+    char a[11];
+    int b[11];
+    for(int i = 0; i < 11; i++ ){
+        printf("El elemento a[%d] tiene la direccion : %x\n", i, &a[i]);
+    }
+    for(int i = 0; i < 11; i++ ){
+        b[i] = 1000 + i ;
+        printf("El elemento b[%d] tiene el valor guardado de : %d\n", i, b[i]);
+    }
+    return 0; 
 }
