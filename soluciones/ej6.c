57f0580a1f8ccf9457352836cc03fb2b8101dd69
adidmccc <adieldan.1912@gmail.com>
2026-02-24 20:54:12 -0600

    Ejercicio6

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,13 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 6
-    return 0;
+int main (){
+    int m[2][4];
+
+for (int i = 0; i < 2; i++){
+    for (int j = 0; j < 4; j++){
+         printf("[%p]", &m[i][j]);
+    }
+    } 
 }
+/*
+- lo almacena por filas
+*/
