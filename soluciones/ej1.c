33e949eddd9efe7e9ca990e030dc8d72834b69f7
adidmccc <adieldan.1912@gmail.com>
2026-02-24 20:38:57 -0600

    Ejercicio1
    Modified main function to print addresses of array elements.

--- soluciones/ej1.c
+++ soluciones/ej1.c
@@ -1,6 +1,10 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 1
-    return 0;
+
+int main() {
+
+    int a[6];
+
+    for (int i = 0; i < 6; i++){
+        printf("%p\n",&a[i]);
+    } 
 }
